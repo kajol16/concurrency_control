0d8035025aab740b811970c3ea7642acec01fa1b
kajol16 <77875486+kajol16@users.noreply.github.com>
2021-07-26 17:54:40 -0500

    Add files via upload

---
+++ op.py
@@ -0,0 +1,16 @@
+OFF_IP_F_NAME = 1
+OFF_OP_F_NAME = 2
+
+
+READ = 'r'
+WRITE = 'w'
+O_T_ID = 1
+OFFS_OPERATION = 0
+BEGIN = 'b'
+END = 'e'
+DELETE_IGNORE = 3
+IGNORE = 2
+SUCCESS = 1
+ABORT = 0
+ABORTED = 4
+DLine = '\n-------------------------------------------------------------------'
